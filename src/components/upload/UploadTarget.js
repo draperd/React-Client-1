5d5028190cf12501f7a8e7b492a1ddfe46902b69
ddraper <david.draper@alfresco.com>
2017-01-31 15:25:46 +0000

    Updates (primarily JSDoc) for uploading

--- src/components/upload/UploadTarget.js
+++ src/components/upload/UploadTarget.js
@@ -6,10 +6,20 @@ import xhr from "../../utilities/Xhr";
 import { collectionEvents } from "../containers/Collection";
 import { merge, clone } from "lodash";
 
+/**
+ * 
+ * @static
+ * @type {object}
+ * @property {string} ADDED The file has been added and is pending upload
+ * @property {string} FAILURE The file could not be uploaded
+ * @property {string} SUCCESS The file was successfully uploaded
+ * @property {string} UPLOADING The file is currently uploading
+ */
 const uploadState = {
    ADDED: "added",
    FAILURE: "failure",
-   SUCCESS: "success"
+   SUCCESS: "success",
+   UPLOADING: "uploading"
 };
 
 /**
@@ -33,80 +43,110 @@ class UploadTarget extends React.Component {
       this._numUploadsInProgress = 0;
       this.totalNewUploads = 0;
 
+      // NOTE: It is necessary to bind in the constructor rather than on registration in order
+      //       that the event listeners can be removed
+      //       See: https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener
+      this.onDrop = this.onDrop.bind(this);
+      this.onDrageEnter = this.onDragEnter.bind(this);
+      this.onDragOver = this.onDragOver.bind(this);
    }
 
+   /**
+    * 
+    * @instance
+    */
    componentDidMount() {
-      this.supportsDNDUpload = ("draggable" in document.createElement("span"));
       this.addUploadSuport();
    }
 
+   /**
+    * Adds event listeners for the drop, dragenter and dragover events when drag-and-drop
+    * is supported by the browser.
+    * 
+    * @instance
+    */
    addUploadSuport() {
-      if (this.supportsDNDUpload)
+      if ("draggable" in document.createElement("span"))
       {
          this.dndUploadEnabled = true;
-         this.refs.componentNode.addEventListener("drop", this.onDrop.bind(this));
-         this.refs.componentNode.addEventListener("dragenter", this.onDragEnter.bind(this));
-         this.refs.componentNode.addEventListener("dragover", this.onDragOver.bind(this));
+         this.refs.componentNode.addEventListener("drop", this.onDrop);
+         this.refs.componentNode.addEventListener("dragenter", this.onDragEnter);
+         this.refs.componentNode.addEventListener("dragover", this.onDragOver);
       }
    }
 
+   /**
+    * Removes event listeners for the drop, dragenter and dragover events
+    * 
+    * @instance
+    */
    removeUploadSupport() {
-
+      this.refs.componentNode.removeEventListener("drop", this.onDrop);
+      this.refs.componentNode.removeEventListener("dragenter", this.onDragEnter);
+      this.refs.componentNode.removeEventListener("dragover", this.onDragOver);
    }
 
+   /**
+    * 
+    * @instance
+    * @param {object} event The drag over event
+    */
    onDragEnter(event) {
-      console.info("Drag enter");
+      
    }
 
+   /**
+    * Prevents further propagation of the drag over event to ensure that drop events
+    * can be properly handled.
+    * 
+    * @instance
+    * @param {object} event The drag over event
+    */
    onDragOver(event) {
-      console.info("Drag over");
       event.stopPropagation();
       event.preventDefault();
    }
 
+   /**
+    * 
+    * @instance
+    * @param {object} event The drop event
+    */
    onDrop(event) {
-      console.info("Drop event !");
-      try 
+      // Only perform a file upload if the user has *actually* dropped some files!
+      if (event.dataTransfer.files !== undefined && 
+          event.dataTransfer.files !== null && 
+          event.dataTransfer.files.length > 0)
       {
-         // Only perform a file upload if the user has *actually* dropped some files!
-         if (event.dataTransfer.files !== undefined && 
-             event.dataTransfer.files !== null && 
-             event.dataTransfer.files.length > 0)
-         {
-            let destination = this._currentNode ? this._currentNode.nodeRef : null;
-            let defaultConfig = {
-               destination: destination,
-               siteId: null,
-               containerId: null,
-               uploadDirectory: null,
-               description: "",
-               overwrite: false,
-               thumbnails: "doclib",
-               username: null,
-               relativePath: this.props.relativePath || "/"
-            };
-            this.onUploadRequest({
-               files: event.dataTransfer.files,
-               targetData: defaultConfig
-            });
-         }
-         else
-         {
-            // Error handling?
-         }
+         let uploadConfig = {
+            description: "",
+            overwrite: false,
+            thumbnails: "doclib",
+            username: null,
+            relativePath: this.props.relativePath || "/"
+         };
+         this.onUploadRequest({
+            files: event.dataTransfer.files,
+            targetData: uploadConfig
+         });
       }
-      catch(exception)
+      else
       {
-         
+         // Error handling?
       }
 
       event.stopPropagation();
       event.preventDefault();
    }
 
+   /**
+    * 
+    * @instance
+    * @param {object}   input
+    * @param {FileList} input.files The files to upload
+    * @param {object}   input.targetData The basic upload configuration
+    */
    onUploadRequest(input) {
-
-      // Make sure we have enough information to continue
       if (input.files && input.targetData) 
       {
          this.totalNewUploads += input.files.length;
@@ -114,16 +154,21 @@ class UploadTarget extends React.Component {
       }
    }
 
-   constructUploadData(input) {
+   /**
+    * 
+    * @instance
+    * @param  {object} input
+    * @param  {File}   input.file The file to construct the upload configuration for
+    * @param  {string} input.fileName The name of the file to be uploaded
+    * @param  {object} input.targetData The base configuration for uploading
+    * @return {object} The upload configuration specific to the supplied file data.
+    */
+   createUploadConfig(input) {
       return {
          filedata: input.file,
          filename: input.fileName,
-         destination: input.targetData.destination,
-         siteId: input.targetData.siteId,
-         containerId: input.targetData.containerId,
          relativePath: this.props.relativePath || "/",
          majorVersion: input.targetData.majorVersion ? input.targetData.majorVersion : "true",
-         updateNodeRef: input.targetData.updateNodeRef,
          description: input.targetData.description,
          overwrite: input.targetData.overwrite,
          thumbnails: input.targetData.thumbnails,
@@ -131,6 +176,12 @@ class UploadTarget extends React.Component {
       };
    }
 
+   /**
+    * 
+    * 
+    * @param  {[type]} input [description]
+    * @return {[type]}       [description]
+    */
    createUploadRequest(input) {
       // Add the data to the upload property of XMLHttpRequest so that we can determine which file each
       // progress update relates to (the event argument passed in the progress function does not contain
@@ -146,7 +197,7 @@ class UploadTarget extends React.Component {
 
       // Construct an object containing the data required for file upload
       // Note that we use .name and NOT .fileName which is non-standard and will break FireFox 7
-      let uploadData = this.constructUploadData({
+      let uploadData = this.createUploadConfig({
          file: input.file, 
          fileName: input.file.name, 
          targetData: input.targetData
@@ -162,11 +213,16 @@ class UploadTarget extends React.Component {
       };
    }
 
-
+   /**
+    * 
+    * @instance
+    * @param {object}   input
+    * @param {FileList} input.files The files to upload
+    * @param {object}   input.targetData The basic upload configuration
+    */
    startUploads(input) {
-
+      // Set up the request and configuration for each file...
       Object.keys(input.files).forEach(function(key) {
-         // Ensure a unique file ID
          let fileId = Date.now();
          while (this.fileStore.hasOwnProperty(fileId)) 
          {
@@ -180,12 +236,7 @@ class UploadTarget extends React.Component {
          });
       }, this);
       
-      // Start uploads
-      this.spawnFileUploads();
-   }
-
-
-   spawnFileUploads() {
+      // Start uploading...
       Object.keys(this.fileStore).forEach(function(fileId) {
          var fileInfo = this.fileStore[fileId];
          if (fileInfo.state === uploadState.ADDED) {
@@ -194,17 +245,20 @@ class UploadTarget extends React.Component {
       }, this);
    }
 
+   /**
+    * Starts uploading the supplied file.
+    * 
+    * @instance
+    * @param  {object} fileInfo The details of the file to upload
+    */
    startFileUpload(fileInfo) {
       // TODO: Ensure we only upload the maximum allowed at a time
-
       this._numUploadsInProgress++;
 
-      fileInfo.state = this.STATE_UPLOADING;
+      fileInfo.state = uploadState.UPLOADING;
 
-      let formData = new FormData();
       let uploadData = fileInfo.uploadData;
-      
-      // Set-up the form data object
+      let formData = new FormData();
       formData.append("fileData", uploadData.filedata);
       formData.append("fileName", uploadData.filename);
       formData.append("autoRename", !uploadData.overwrite);
@@ -222,6 +276,12 @@ class UploadTarget extends React.Component {
       fileInfo.request.send(formData);
    }
 
+   /**
+    * 
+    * @instance
+    * @param  {string} fileId The unique id of the file upload
+    * @param  {object} event  The upload progress event for the file
+    */
    uploadProgressListener(fileId, event) {
       let fileInfo = this.fileStore[fileId];
       if (fileInfo && event.lengthComputable) 
@@ -237,6 +297,51 @@ class UploadTarget extends React.Component {
       }
    }
 
+   /**
+    * Calculates the overall progress of the uploads that are currently ongoing
+    * 
+    * @instance
+    */
+   updateAggregateProgress() {
+      let fileIds = Object.keys(this.fileStore);
+      let totalPercent = this.totalNewUploads * 100;
+      let cumulativeProgress = 0;
+      let inProgressFiles = 0;
+
+      // Run through all uploads, calculating total and current progress
+      fileIds.forEach(function(fileId) {
+         var fileInfo = this.fileStore[fileId];
+         if (fileInfo.state === uploadState.ADDED || fileInfo.state === uploadState.UPLOADING) 
+         {
+            cumulativeProgress += fileInfo.progress;
+            inProgressFiles++;
+         }
+      }, this);
+
+      // Add completed files to the cumulative total
+      cumulativeProgress += (this.totalNewUploads - inProgressFiles) * 100;
+
+      // Calculate total percentage and send to widget
+      // NOTE: If no in-progress files, or race-condition causes zero total percent, then
+      // just call it 100, because it will mean that essentially there are no pending uploads
+      var currentProgressPercent = (inProgressFiles && totalPercent) ? Math.floor(cumulativeProgress / totalPercent * 100) : 100;
+      this.uploadDisplayWidget.updateAggregateProgress(currentProgressPercent / 100);
+
+      // If no longer have uploads pending, update the total-completed variable
+      if (currentProgressPercent === 100) 
+      {
+         this.resetTotalUploads();
+      }
+
+      // TODO: Display status...
+   }
+
+   /**
+    * 
+    * @instance
+    * @param {string} fileId The unique id of the file that has been uploaded
+    * @param {object} event The event captured as a result of the upload success.
+    */
    successListener(fileId, event) {
       let fileInfo = this.fileStore[fileId];
       if (fileInfo) {
@@ -261,6 +366,12 @@ class UploadTarget extends React.Component {
       }
    }
 
+   /**
+    * 
+    * @instance
+    * @param {string} fileId The unique id of the file that has failed to upload
+    * @param {object} event The event captured as a result of the upload failure.
+    */
    failureListener(fileId, event) {
       let fileInfo = this.fileStore[fileId];
       if (fileInfo) 
@@ -272,6 +383,12 @@ class UploadTarget extends React.Component {
       }
    }
 
+   /**
+    * 
+    * @instance
+    * @param {string} fileId The unique id of the file that has finished uploading
+    * @param {object} event The event captured as a result of the upload completion.
+    */
    processUploadCompletion(fileId, event) {
       let fileInfo = this.fileStore[fileId];
       let responseCode = fileInfo.request.status;
@@ -292,6 +409,12 @@ class UploadTarget extends React.Component {
       }
    }
 
+   /**
+    * 
+    * @instance
+    * @param {string} fileId The unique id of the file that has finished (or failed) uploading
+    * @param {object} event The event captured as a result of the upload failure.
+    */
    processUploadFailure(fileId, event) {
       let fileInfo = this.fileStore[fileId];
       if (fileInfo) 
@@ -302,7 +425,14 @@ class UploadTarget extends React.Component {
       }
    }
 
-   onUploadFinished() {
+   /**
+    * Handles the completion of an upload (successful or otherwise) and emits an
+    * event to indicate that a new item has been created.
+    * 
+    * @instance
+    * @param {string} fileId The unique id of the file that has finished (or failed) uploading
+    */
+   onUploadFinished(fileId) {
       this._numUploadsInProgress--;
 
       var changeEvent = new CustomEvent(collectionEvents.ITEM_CREATED, {
@@ -311,6 +441,11 @@ class UploadTarget extends React.Component {
       this.refs.componentNode.dispatchEvent(changeEvent);
    }
 
+   /**
+    * 
+    * @instance
+    * @return {JSX}
+    */
    render() {
       const childrenWithProps = React.Children.map(this.props.children, (child) => React.cloneElement(child, {
          orderBy: this.props.orderBy,
