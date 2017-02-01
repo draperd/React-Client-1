7e6cee9695b04ebe332941ba693c15b6b02435dd
ddraper <david.draper@alfresco.com>
2017-02-01 10:11:29 +0000

    Correction to previous commit

--- src/routes/Nodes.js
+++ src/routes/Nodes.js
@@ -35,7 +35,6 @@ const Nodes = React.createClass({
                         orderBy="name"
                         include="properties"
                         includeSource="true" >
-                        useHash="true">
                   
                   <Toolbar>
                      <Menu label="Create...">
