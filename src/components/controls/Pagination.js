0bb0e6f4121d242697c90896d426722e1d19b8fd
ddraper <david.draper@alfresco.com>
2017-02-02 15:06:38 +0000

    Pagination fixes (ensure page selection menu opens)

--- src/components/controls/Pagination.js
+++ src/components/controls/Pagination.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { collectionEvents } from "../containers/Collection";
+import { uniqueId } from "lodash";
 
 const paginationStyle = {
    outerSpan: {
@@ -10,6 +11,15 @@ const paginationStyle = {
 };
 
 class Pagination extends React.Component {
+
+   constructor(props) {
+      super(props);
+      this.id = props.id || uniqueId("components_controls_Pagination__");
+   }
+
+   componentDidMount() {
+      window.componentHandler.upgradeDom();
+   }
 
    pageBack() {
       let changeEvent = new CustomEvent(collectionEvents.PAGE_BACKWARDS, {
@@ -40,21 +50,20 @@ class Pagination extends React.Component {
 
          <span>{this.props.list.pagination.maxItems}</span>
 
-         <button id="paginationItemsPerPage"
-                 className="mdl-button mdl-js-button mdl-button--icon" 
-                 onClick={this.pageBack.bind(this)}>
+         <button id={this.id}
+                 className="mdl-button mdl-js-button mdl-button--icon">
             <i className="material-icons">arrow_drop_down</i>
          </button>
 
          <ul className="mdl-menu mdl-menu--bottom-right mdl-js-menu mdl-js-ripple-effect"
-             htmlFor="paginationItemsPerPage">
+             htmlFor={this.id}>
             <li className="mdl-menu__item" onClick={() => this.updateMaxItems(5)}>5</li>
             <li className="mdl-menu__item" onClick={() => this.updateMaxItems(10)}>10</li>
             <li className="mdl-menu__item" onClick={() => this.updateMaxItems(20)}>20</li>
             <li className="mdl-menu__item" onClick={() => this.updateMaxItems(50)}>50</li>
          </ul>
 
-         <span>{this.props.list.pagination.skipCount + 1} - {this.props.list.pagination.skipCount + this.props.list.pagination.maxItems} of {this.props.list.pagination.totalItems}</span>
+         <span>{this.props.list.pagination.skipCount + 1} - {this.props.list.pagination.skipCount + this.props.list.pagination.count} of {this.props.list.pagination.totalItems}</span>
 
          <button className="mdl-button mdl-js-button mdl-button--icon" 
                  disabled={this.props.list.pagination.skipCount ? false : true} 
