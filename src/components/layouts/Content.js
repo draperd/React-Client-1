6d2da67670f600a5d3d279a87c6818be8d69b33e
kevinroast <kevin.roast@alfresco.com>
2017-02-08 10:24:26 +0000

    Add page-content level CSS Scope CSS to page-content for main app pages

--- src/components/layouts/Content.js
+++ src/components/layouts/Content.js
@@ -18,7 +18,7 @@ class Content extends React.Component {
    render() {
       return (
          <main className="mdl-layout__content">
-            <div className="page-content" data-dojo-attach-point="contentNode">
+            <div className="page-content" id={this.props.htmlid}>
                {this.props.children}
             </div>
          </main>
