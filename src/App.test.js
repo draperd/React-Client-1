8a391612814841976c508c9ac6085c70491d3495
ddraper <david.draper@alfresco.com>
2017-02-10 15:30:29 +0000

    Removed original test from CLI

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Collection from "./components/containers/Collection";
-
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<Collection />, div);
-});
