8376b41fb5f51db36d87cb027736a9a40e184751
Gregory Melahn <greg.melahn@alfresco.com>
2017-04-07 08:28:40 -0400

    added some more language files

--- src/index.js
+++ src/index.js
@@ -5,23 +5,39 @@ import { browserHistory, Router, Route, IndexRoute } from "react-router";
 import { IntlProvider, addLocaleData } from "react-intl";
 
 // It is necessary to import all of the locales that you wish to support...
+import de from "react-intl/locale-data/de";
 import en from "react-intl/locale-data/en";
 import es from "react-intl/locale-data/es";
 import fr from "react-intl/locale-data/fr";
-addLocaleData([...en, ...es, ...fr]); // Add additional locale data as necessary
+import it from "react-intl/locale-data/it";
+import ja from "react-intl/locale-data/ja";
+import nb from "react-intl/locale-data/nb";
+import nl from "react-intl/locale-data/nl";
+import pt from "react-intl/locale-data/pt";
+import ru from "react-intl/locale-data/ru";
+import zh from "react-intl/locale-data/zh";
+addLocaleData([...de, ...en, ...es, ...fr, ...it, ...ja, ...nb, ...nl, ...pt, ...ru, ...zh]); // Add additional locale data as necessary
 
 // Load the global messages for the locales that you want to support, individual
 // components will add their own messages on demand (add additional bundles as necessary)
+import deMessages from "./i18n/de.json";
 import enMessages from "./i18n/en.json";
 import esMessages from "./i18n/es.json";
-
+import frMessages from "./i18n/fr.json";
+import itMessages from "./i18n/it.json";
+import jaMessages from "./i18n/ja.json";
+import nbMessages from "./i18n/nb.json";
+import nlMessages from "./i18n/nl.json";
+import ptMessages from "./i18n/pt_BR.json";
+import ruMessages from "./i18n/ru.json";
+import zhMessages from "./i18n/zh_CN.json";
 
 // Determine user language. Different browsers have the user locale defined
 // on different fields on the "navigator" object, so we make sure to account
 // for these different by checking all of them
 const language = (navigator.languages && navigator.languages[0]) ||
-                  navigator.language ||
-                  navigator.userLanguage;
+    navigator.language ||
+    navigator.userLanguage;
 
 // Split locales with a region code (only necessary when selecting language from data)...
 const languageWithoutRegionCode = language.toLowerCase().split(/[_-]+/)[0];
@@ -31,14 +47,41 @@ const languageWithoutRegionCode = language.toLowerCase().split(/[_-]+/)[0];
 
 let messages = enMessages;
 switch (languageWithoutRegionCode) {
-   case "en":
-      messages = enMessages;
-      break;
-   case "es":
-      messages = esMessages;
-      break;
-   default:
-      break;
+    case "de":
+        messages = deMessages;
+        break;
+    case "en":
+        messages = enMessages;
+        break;
+    case "es":
+        messages = esMessages;
+        break;
+    case "fr":
+        messages = frMessages;
+        break;
+    case "it":
+        messages = itMessages;
+        break;
+    case "ja":
+        messages = jaMessages;
+        break;
+    case "nb":
+        messages = nbMessages;
+        break;
+    case "nl":
+        messages = nlMessages;
+        break;
+    case "pt":
+        messages = ptMessages;
+        break;
+    case "ru":
+        messages = ruMessages;
+        break;
+    case "zh":
+        messages = zhMessages;
+        break;
+    default:
+        break;
 }
 
 
@@ -69,36 +112,36 @@ import Tags from "./routes/Tags";
 import LiveSearch from "./routes/LiveSearch";
 import Search from "./routes/Search";
 
-// A simple function used to ensure that the main routes cannot be accessed by 
+// A simple function used to ensure that the main routes cannot be accessed by
 // unauthenticated users. If the user is not logged in then they will be automatically
 // redirected to the login page...
 function requireAuth(nextState, replace) {
-   if (!auth.loggedIn()) {
-      replace({
-         pathname: "/login",
-         state: { nextPathname: nextState.location.pathname }
-      });
-   }
+    if (!auth.loggedIn()) {
+        replace({
+            pathname: "/login",
+            state: { nextPathname: nextState.location.pathname }
+        });
+    }
 }
 
 // This sets up the routes for the application...
 // You can add your own routes within the MainLayout (if you want a link to your route
 // to appear in the navigation drawer then you should edit MainLayout.js to add the link)....
 render((
-   <IntlProvider locale={language} messages={messages} >
-      <Router history={browserHistory}>
-         <Route path="login" component={Login} />
-         <Route path="/" component={MainLayout} onEnter={requireAuth}>
-            <IndexRoute component={Nodes} onEnter={requireAuth} />
-            <Route path="nodes" component={Nodes} onEnter={requireAuth} />
-            <Route path="livesearch" component={LiveSearch} onEnter={requireAuth} />
-            <Route path="users" component={Users} onEnter={requireAuth} />
-            <Route path="filmstrip" component={FilmStrip} onEnter={requireAuth} />
-            <Route path="sites" component={Sites} onEnter={requireAuth} />
-            <Route path="tags" component={Tags} onEnter={requireAuth} />
-            <Route path="search" component={Search} onEnter={requireAuth} />
-            <Route path="node/:id" component={NodeDetails} onEnter={requireAuth} />
-         </Route>
-      </Router>
-   </IntlProvider>
+    <IntlProvider locale={language} messages={messages} >
+    <Router history={browserHistory}>
+    <Route path="login" component={Login} />
+    <Route path="/" component={MainLayout} onEnter={requireAuth}>
+    <IndexRoute component={Nodes} onEnter={requireAuth} />
+    <Route path="nodes" component={Nodes} onEnter={requireAuth} />
+    <Route path="livesearch" component={LiveSearch} onEnter={requireAuth} />
+    <Route path="users" component={Users} onEnter={requireAuth} />
+    <Route path="filmstrip" component={FilmStrip} onEnter={requireAuth} />
+    <Route path="sites" component={Sites} onEnter={requireAuth} />
+    <Route path="tags" component={Tags} onEnter={requireAuth} />
+    <Route path="search" component={Search} onEnter={requireAuth} />
+    <Route path="node/:id" component={NodeDetails} onEnter={requireAuth} />
+    </Route>
+    </Router>
+    </IntlProvider>
 ), document.getElementById('root'))
